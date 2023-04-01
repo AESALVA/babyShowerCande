b6c4b9a12bc7cceec2238c81f20432a5e8557b78
AESalva <eduardo_salva@hotmail.com>
2023-03-31 23:48:58 -0300

    logo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import CandelariaLogo from './assets/CandelariaLogo.jpg';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Baby Shower Candelaria</h1>
+      <div className='Logo'>
+        <img src={CandelariaLogo} alt="CandelariaLogo"  />
+      </div>
     </div>
   )
 }
