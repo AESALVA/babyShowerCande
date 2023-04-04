0c8b6cb965068f1e63516bbc9551dc6d707d18a6
AESalva <eduardo_salva@hotmail.com>
2023-04-03 21:04:40 -0300

    footer features

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../Styles/footer.css';
 import CandelariaLogo from '../assets/CandelariaLogo.jpg';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars,  faHouse, faUser, faFolderOpen, faEnvelopeCircleCheck } from "@fortawesome/free-solid-svg-icons";
+import Nav from 'react-bootstrap/Nav';
 
 const Footer = () => {
   return (
@@ -8,6 +11,13 @@ const Footer = () => {
       <div className='Logo'>
         <img src={CandelariaLogo} alt="CandelariaLogo"  />
       </div>
+      <ul className='FooterNav'>
+      <li><Nav.Link href="/">Home</Nav.Link></li>
+        <li><Nav.Link href="#Register">Register</Nav.Link></li>
+        <li><Nav.Link href="#About">About</Nav.Link></li>
+        <li><Nav.Link href="#Contact">Contact</Nav.Link></li>
+      </ul>
+      <p>Copyright © 2023 Angel Eduardo Salva</p>
     </div>
   )
 }
