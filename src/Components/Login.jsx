39b7003d3e9f1c072ca6f9bc1eae457a513b888f
AESalva <eduardo_salva@hotmail.com>
2023-04-08 21:06:42 -0300

    add validations to inputs on login with context

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,66 +1,98 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
-import '../Styles/login.css';
-import Nav from 'react-bootstrap/Nav';
+import "../Styles/login.css";
+import Nav from "react-bootstrap/Nav";
 import { NavLink } from "react-router-dom";
-import { useUserContext, useLoadedContext } from '../UserProvider';
-
-
-
-
+import { useUserContext, useLoadedContext, useValidationContext } from "../UserProvider";
 
 const Login = () => {
+  const auth = useUserContext();
 
-const user= useUserContext();
+  const Load = useLoadedContext();
 
-const Load = useLoadedContext();
-
-const person = {name:"Eduardo",pass:"Eduardo1",role:"admin"}
-
-const handleClick = ()=>{
-  user.updateAuth(person.name,person.pass,person.role);
-  Load.setIsLoaded(true);
-}
-
-useEffect(() => {
-console.log(user.auth)
-console.log(Load.isLoaded)
-}, [user])
+  const Validation = useValidationContext();
 
 
+  const [password, setPassword] = useState("");
+  const [mail, setMail] = useState("");
+  const [firstPassword, setFirstPassword] = useState(true);
+  const [firstMail, setFirstMail] = useState(true);
+  const [wrongCredentials, setWrongCredentials] = useState("");
 
+  
+  const handleClick = () => {
+    auth.setAuth({ name: mail, pass: password, role: "user" });
+  };
 
+  useEffect(() => {
+    console.log(auth.auth)
+  }, [auth])
+  
 
-  return (<>
-    <div className='LoginContainer'>
-      <div className='form-box login'>
-        <h2>Login</h2>
-        <form action="#">
-          <div className='input-box'>
-            <span className='icon'><FontAwesomeIcon icon={faEnvelope} /></span>
-            <input type="text " required />
-            <label>Email</label>
-          </div>
-          <div className='input-box'>
-            <span className='icon'><FontAwesomeIcon icon={faLock} /></span>
-            <input type="password" required />
-            <label>Password</label>
-          </div>
-          <div className='forgotPass'>
-            <NavLink to='/babyShowerCande/PassRecovery'>Forgot password ?</NavLink>
-          </div>
-          <div onClick={()=>handleClick()} className='btn'>
-          <Nav.Link >Login</Nav.Link>
-          </div>
-          <div className='loginRegister'>
-            <p>Don't have an account?</p><NavLink to='/babyShowerCande/Register' className='registerLink'>Register</NavLink>
-          </div>
-        </form>
+  return (
+    <>
+      <div className="LoginContainer">
+        <div className="form-box login">
+          <h2>Login</h2>
+          <form action="#">
+            <div className="input-box">
+              <span className="icon">
+                <FontAwesomeIcon icon={faEnvelope} />
+              </span>
+              <input
+                type="text"
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
+                onBlur={() => setFirstMail(false)}
+                required
+              />
+              <label>Email{' '}{!Validation.validateMail(mail) && !firstMail && (
+                  <span className="text-danger">
+                    You must complete this field
+                  </span>
+                )}</label>
+            </div>
+            <div className="input-box">
+              <span className="icon">
+                <FontAwesomeIcon icon={faLock} />
+              </span>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onBlur={() => setFirstPassword(false)}
+                required
+              />
+              <label>
+                Password{" "}
+                {!Validation.validatePassword(password) && !firstPassword && (
+                  <span className="text-danger">
+                    You must complete this field
+                  </span>
+                )}
+              </label>
+            </div>
+            <span className="text-danger d-flex justify-content-center">{wrongCredentials}</span>
+            <div className="forgotPass">
+              <NavLink to="/babyShowerCande/PassRecovery">
+                Forgot password ?
+              </NavLink>
+            </div>
+            <div onClick={() => handleClick()} className="btn">
+              <Nav.Link>Login</Nav.Link>
+            </div>
+            <div className="loginRegister">
+              <p>Don't have an account?</p>
+              <NavLink to="/babyShowerCande/Register" className="registerLink">
+                Register
+              </NavLink>
+            </div>
+          </form>
+        </div>
       </div>
-      </div>
-      </>
-  )
-}
+    </>
+  );
+};
 
-export default Login
+export default Login;
--- src/UserProvider.jsx
+++ src/UserProvider.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useContext } from "react";
+import validator from "validator";
+
 
 const userContext = React.createContext();
 const LoadedContext = React.createContext();
+const ValidationContext = React.createContext();
 
 export function useUserContext() {
   return useContext(userContext);
@@ -11,21 +14,50 @@ export function useLoadedContext() {
   return useContext(LoadedContext);
 }
 
-const UserProvider = ({children}) => {
+export function useValidationContext() {
+  return useContext(ValidationContext);
+}
 
+const UserProvider = ({ children }) => {
   const [auth, setAuth] = useState({ name: "", pass: "", role: "" });
-
-  const updateAuth = (u,p,r)=>{
-    setAuth({name:u,pass:p,role:r})
-  }
 
   const [isLoaded, setIsLoaded] = useState(false);
 
+  const validatePassword = (n = "hol") => {
+    return (
+      validator.isLength(n, { min: 8, max: 20 }) &&
+      validator.isStrongPassword(n, {
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 0,
+      })
+    );
+  };
+
+  const validateMail = (n) => {
+    return (
+      validator.isLength(n, { min: 5, max: 36 }) &&
+      validator.isEmail(n, {
+        allow_display_name: false,
+        require_display_name: false,
+        allow_utf8_local_part: true,
+        require_tld: true,
+        allow_ip_domain: false,
+        domain_specific_validation: false,
+        blacklisted_chars: "",
+        host_blacklist: [],
+      })
+    );
+  };
+
   return (
-    <userContext.Provider value={{auth,updateAuth}} >  
-        <LoadedContext.Provider value={{isLoaded,setIsLoaded}}>
-        {children}
-        </LoadedContext.Provider>
+    <userContext.Provider value={{ auth, setAuth }}>
+      <LoadedContext.Provider value={{ isLoaded, setIsLoaded }}>
+        <ValidationContext.Provider value={{ validatePassword, validateMail }}>
+          {children}
+        </ValidationContext.Provider>
+      </LoadedContext.Provider>
     </userContext.Provider>
   );
 };
