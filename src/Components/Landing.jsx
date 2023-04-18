558cdc526ddbd6656dbcc1329b766c2e9a615349
AESalva <eduardo_salva@hotmail.com>
2023-04-18 16:53:53 -0300

    add carousel landing

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import '../Styles/landing.css'
 
 const Landing = () => {
+
+
+
   return (<>
     <div className='ContainerLanding'>
     <h1>¡¡ Bienvenidos !!</h1>
+<div className="media-scroller snaps-inline">
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+  <div className="media-element"><img src='' alt=''/></div>
+</div>
     </div>
     </>
   )
