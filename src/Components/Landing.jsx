b6fdc7d460b682055183c670f5bc31f0a0e63e62
AESalva <eduardo_salva@hotmail.com>
2023-04-18 17:06:00 -0300

    carousel features

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
 
   return (<>
     <div className='ContainerLanding'>
-    <h1>¡¡ Bienvenidos !!</h1>
+    <h1 className='LandingTitle'>¡¡ Bienvenidos !!</h1>
 <div className="media-scroller snaps-inline">
   <div className="media-element"><img src='' alt=''/></div>
   <div className="media-element"><img src='' alt=''/></div>
