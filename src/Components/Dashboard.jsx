edc42bf99a58b61ffd9abf72c3da29005708f4c3
AESalva <eduardo_salva@hotmail.com>
2023-04-20 18:23:01 -0300

    test

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -28,7 +28,7 @@ const Dashboard = () => {
     fetch("https://babyshowerback.vercel.app/Comments/all")
     .then((res) => res.json())
     .then((json) => setContainerMessages(json));
-  }, [])
+  }, [auth.auth])
   
 
   return (
