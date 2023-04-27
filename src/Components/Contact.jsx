4c483e16402ef4d050d581d26c8e5cede63baed2
AESalva <eduardo_salva@hotmail.com>
2023-04-27 18:25:09 -0300

    features contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,12 +6,17 @@ const Contact = () => {
       <h1 className="title">Contacto</h1>
       <div className="text">
         <h3>
-          ¡Gracias por visitar nuestra página web y si están interesados en realizar un obsequio,
-          para Candelaria por favor contáctenos a través de nuestro correo electrónico
-          (eduardo_salva@hotmail.com). También pueden utilizar
-          la sección de mensajes en nuestro muro para expresar sus buenos deseos
-          para Candelaria y su familia. Agradecemos su generosidad y apoyo en la
-          celebración de este hermoso momento. ¡Saludos!
+          ¡Gracias por visitar nuestra página web y si están interesados en
+          realizar un obsequio para Candelaria por favor contáctenos a través
+          de nuestro correo electrónico (angy.go09@gmail.com) o (eduardo_salva@hotmail.com).
+        </h3>
+        <h3>
+          También pueden utilizar la sección de mensajes en nuestro muro para
+          expresar sus buenos deseos para Candelaria y su familia.
+        </h3>
+        <h3>
+          Agradecemos su generosidad y apoyo en la celebración de este hermoso
+          momento. ¡Saludos!
         </h3>
       </div>
     </div>
